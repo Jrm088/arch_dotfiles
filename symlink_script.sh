8db308bcd5c1f42c31ba93cdea5fd7d191cc65fa
Jrm088 <12345678+yourusername@users.noreply.github.com>
2025-11-18 18:39:44 -0500

    updated symlink script

--- symlink_script.sh
+++ symlink_script.sh
@@ -6,37 +6,19 @@ if [ "$EUID" -eq 0 ]; then
    exit 1
 fi
 
-echo "Creating dotfiles directory structure..."
+echo "Creating config directories..."
 
-# Create main dotfiles directory
-mkdir -p ~/dotfiles
+# Create config directories if they don't exist
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/kitty
+mkdir -p ~/.config/picom
+mkdir -p ~/.config/polybar
+mkdir -p ~/.config/rofi
+mkdir -p ~/.config/fastfetch
 
-# Create subdirectories
-mkdir -p ~/dotfiles/fastfetch
-mkdir -p ~/dotfiles/i3
-mkdir -p ~/dotfiles/kitty
-mkdir -p ~/dotfiles/picom
-mkdir -p ~/dotfiles/polybar
-mkdir -p ~/dotfiles/rofi
-mkdir -p ~/dotfiles/starship
-mkdir -p ~/dotfiles/vim
-mkdir -p ~/dotfiles/wallpapers
+echo "Creating symlinks from dotfiles to config locations..."
 
-echo "Moving config files to dotfiles..."
-
-# Move config files to dotfiles (backup originals)
-mv ~/.config/i3/config ~/dotfiles/i3/
-mv ~/.config/kitty/kitty.conf ~/dotfiles/kitty/
-mv ~/.config/picom/picom.conf ~/dotfiles/picom/
-mv ~/.config/polybar/config.ini ~/dotfiles/polybar/
-mv ~/.config/rofi/config.rasi ~/dotfiles/rofi/
-mv ~/.config/starship.toml ~/dotfiles/starship/
-mv ~/.vimrc ~/dotfiles/vim/
-mv ~/.config/fastfetch/config.jsonc ~/dotfiles/fastfetch/
-
-echo "Creating symlinks..."
-
-# Create symlinks from dotfiles back to config locations
+# Create symlinks from dotfiles to config locations
 ln -sf ~/dotfiles/i3/config ~/.config/i3/config
 ln -sf ~/dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
 ln -sf ~/dotfiles/picom/picom.conf ~/.config/picom/picom.conf
@@ -47,3 +29,5 @@ ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/fastfetch/config.jsonc ~/.config/fastfetch/config.jsonc
 
 echo "Symlinks created successfully!"
+echo "Your dotfiles are now linked and ready to use!"
+
